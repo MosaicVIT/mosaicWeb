11665b525a085e3486b9e4bf7bf863f4ab0782fe
Anushree Jaiswal <anushreejaiswal.srn@gmail.com>
2025-04-06 15:16:11 +0530

    add for you component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import { Background } from "./components/helper/background";
 import Header from "./components/header/header";
 import { HashRouter, Route, Routes } from "react-router-dom";
+import ForYou from "./components/for-you/for-you";
 
 function App() {
-  return (
-    <div className="App">
-      <HashRouter>
-        <Background />
-        <Header />
-        <Routes>
-          <Route path="/" element={<div>Home</div>} />
-          <Route path="/foryou" element={<div>For You</div>} />
-          <Route path="/discovery" element={<div>Discovery</div>} />
-          <Route path="/timeline" element={<div>Timeline</div>} />
-          <Route path="/subscription" element={<div>Search</div>} />
-        </Routes>
-      </HashRouter>
-    </div>
-  );
+	return (
+		<div className="App">
+			<HashRouter>
+				<Background />
+				<Header />
+				<Routes>
+					<Route path="/" element={<div>Home</div>} />
+					<Route path="/foryou" element={<ForYou />} />
+					<Route path="/discovery" element={<div>Discovery</div>} />
+					<Route path="/timeline" element={<div>Timeline</div>} />
+					<Route path="/subscription" element={<div>Search</div>} />
+				</Routes>
+			</HashRouter>
+		</div>
+	);
 }
 
 export default App;
