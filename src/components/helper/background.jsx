98c83d9ebf5a8ba732257a50cf3388c63a45d307
Anushree Jaiswal <anushreejaiswal.srn@gmail.com>
2025-04-06 15:15:11 +0530

    updated styling

--- src/components/helper/background.jsx
+++ src/components/helper/background.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
-
+import React from "react";
+import styled from "styled-components";
 
 export const Background = styled.div`
-
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #000000;
-    background-image: url('${p=> process.env.PUBLIC_URL + '/bg.png'}');
-    background-size: cover;
-    background-position: center;
-    z-index: -1;
-    /* filter: blur(5px); */
-    opacity: 0.5;
-    pointer-events: none;
-    transition: all 0.3s ease-in-out;
-    animation: fadeIn 1s ease-in-out forwards;
-    @keyframes fadeIn {
-        0% {
-            opacity: 0.5;
-        }
-        100% {
-            opacity: 1;
-        }
-    }
-`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: #000000;
+	background-image: url("${(p) => process.env.PUBLIC_URL + "/bg.png"}");
+	background-size: cover;
+	background-position: center;
+	z-index: -1;
+	/* filter: blur(5px); */
+	opacity: 0.5;
+	pointer-events: none;
+	transition: all 0.3s ease-in-out;
+	animation: fadeIn 1s ease-in-out forwards;
+	@keyframes fadeIn {
+		0% {
+			opacity: 0.5;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+`;
